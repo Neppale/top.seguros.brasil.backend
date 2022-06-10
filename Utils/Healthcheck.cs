203b6421ae9b92325f4a7b346e9696396ba6506e
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-06-10 19:28:27 -0300

    fix: removed debug endpoints

--- Utils/Healthcheck.cs
+++ Utils/Healthcheck.cs
@@ -5,28 +5,27 @@ namespace tsb.mininal.policy.engine.Utils
 {
   public abstract class HealthCheck
   {
+    /** <summary> Esta função faz uma checagem da conexão com o banco de dados. </summary>**/
     public static IResult Check(string dbConnectionString)
     {
-            SqlConnection connectionString = new SqlConnection(dbConnectionString);
-           
-            // Clean code? I don't know her.
-            if (!connectionString.QuerySingle<bool>($"SELECT * from Clientes WHERE id_cliente = 1")) return Results.StatusCode(503);
+      SqlConnection connectionString = new SqlConnection(dbConnectionString);
 
-            if (!connectionString.QuerySingle<bool>($"SELECT * from Coberturas  WHERE id_cobertura = 1")) return Results.StatusCode(503);
+      // Clean code? I don't know her.
+      if (!connectionString.QuerySingle<bool>($"SELECT * from Clientes WHERE id_cliente = 1")) return Results.StatusCode(503);
 
-            if (!connectionString.QuerySingle<bool>($"SELECT * from Usuarios WHERE id_usuario = 1")) return Results.StatusCode(503);
+      if (!connectionString.QuerySingle<bool>($"SELECT * from Coberturas  WHERE id_cobertura = 1")) return Results.StatusCode(503);
 
-            if (!connectionString.QuerySingle<bool>($"SELECT * from Veiculos WHERE id_veiculo = 1")) return Results.StatusCode(503);
+      if (!connectionString.QuerySingle<bool>($"SELECT * from Usuarios WHERE id_usuario = 1")) return Results.StatusCode(503);
 
-            if (!connectionString.QuerySingle<bool>($"SELECT * from Terceirizados WHERE id_terceirizado = 1")) return Results.StatusCode(503);
+      if (!connectionString.QuerySingle<bool>($"SELECT * from Veiculos WHERE id_veiculo = 1")) return Results.StatusCode(503);
 
-            if (!connectionString.QuerySingle<bool>($"SELECT * from Apolices WHERE id_apolice = 1")) return Results.StatusCode(503);
+      if (!connectionString.QuerySingle<bool>($"SELECT * from Terceirizados WHERE id_terceirizado = 1")) return Results.StatusCode(503);
 
-            if (!connectionString.QuerySingle<bool>($"SELECT id_cobertura from Coberturas WHERE id_cobertura = 1")) return Results.StatusCode(503);
+      if (!connectionString.QuerySingle<bool>($"SELECT * from Apolices WHERE id_apolice = 1")) return Results.StatusCode(503);
 
+      if (!connectionString.QuerySingle<bool>($"SELECT id_cobertura from Coberturas WHERE id_cobertura = 1")) return Results.StatusCode(503);
 
-
-            return Results.Ok();
-        }
+      return Results.Ok();
+    }
   }
 }
