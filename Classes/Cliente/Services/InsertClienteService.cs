cbfd9b266e50058360f860e1cf10b843c7f73758
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-06-22 14:34:51 -0300

    fix: insert ocorrencia and insert cliente services now skip null properties properly

--- Classes/Cliente/Services/InsertClienteService.cs
+++ Classes/Cliente/Services/InsertClienteService.cs
@@ -9,38 +9,38 @@ static class InsertClienteService
   {
     SqlConnection connectionString = new SqlConnection(dbConnectionString);
 
+    // Fazendo o telefone2 pular a verificação de nulos.
+    string? originalTelefone2 = cliente.telefone2;
+    if (cliente.telefone2 == "" || cliente.telefone2 == null) cliente.telefone2 = "-";
+
+    // Verificando se alguma das propriedades do cliente é nula ou vazia.
+    bool hasValidProperties = NullPropertyValidator.Validate(cliente);
+    if (!hasValidProperties) return Results.BadRequest("Há um campo inválido na sua requisição.");
+
+    // Voltando telefone2 para o valor original.
+    cliente.telefone2 = originalTelefone2;
+
+    // Verificação de CPF
+    bool cpfIsValid = CpfValidation.Validate(cliente.cpf);
+    if (!cpfIsValid) return Results.BadRequest("O CPF informado é inválido.");
+
+    // Verificação de CNH
+    bool cnhIsValid = CnhValidation.Validate(cliente.cnh);
+    if (!cnhIsValid) return Results.BadRequest("O CPF informado é inválido.");
+
+    // Verificação de CEP
+    Task<bool> cepIsValid = CepValidator.Validate(cliente.cep);
+    if (!cepIsValid.Result) return Results.BadRequest("O CEP informado é inválido.");
+
+    // Verificando se o cliente já existe no banco de dados.
+    bool clienteIsValid = ClienteAlreadyExistsValidator.Validate(cliente, dbConnectionString);
+    if (!clienteIsValid) return Results.Conflict("Os dados deste cliente já estão cadastrados no banco de dados.");
+
+    // Criptografando a senha do cliente.
+    cliente.senha = PasswordHasher.HashPassword(cliente.senha);
+
     try
     {
-
-      // Fazendo o telefone2 pular a validação.
-      if (cliente.telefone2 == "" || cliente.telefone2 == null) cliente.telefone2 = "-";
-
-      // Verificando se alguma das propriedades do cliente é nula ou vazia.
-      bool hasValidProperties = NullPropertyValidator.Validate(cliente);
-      if (!hasValidProperties) return Results.BadRequest("Há um campo inválido na sua requisição.");
-
-      // Voltando telefone2 para o valor original.
-      cliente.telefone2 = cliente.telefone2.Replace("-", "");
-
-      // Verificação de CPF
-      bool cpfIsValid = CpfValidation.Validate(cliente.cpf);
-      if (!cpfIsValid) return Results.BadRequest("O CPF informado é inválido.");
-
-      // Verificação de CNH
-      bool cnhIsValid = CnhValidation.Validate(cliente.cnh);
-      if (!cnhIsValid) return Results.BadRequest("O CPF informado é inválido.");
-
-      // Verificação de CEP
-      Task<bool> cepIsValid = CepValidator.Validate(cliente.cep);
-      if (!cepIsValid.Result) return Results.BadRequest("O CEP informado é inválido.");
-
-      // Verificando se o cliente já existe no banco de dados.
-      bool clienteIsValid = ClienteAlreadyExistsValidator.Validate(cliente, dbConnectionString);
-      if (!clienteIsValid) return Results.Conflict("Os dados deste cliente já estão cadastrados no banco de dados.");
-
-      // Criptografando a senha do cliente.
-      cliente.senha = PasswordHasher.HashPassword(cliente.senha);
-
       connectionString.Query<Cliente>("INSERT INTO Clientes (email, senha, nome_completo, cpf, cnh, cep, data_nascimento, telefone1, telefone2) VALUES (@Email, @Senha, @Nome, @Cpf, @Cnh, @Cep, @DataNascimento, @Telefone1, @Telefone2)",
        new { Email = cliente.email, Senha = cliente.senha, Nome = cliente.nome_completo, Cpf = cliente.cpf, Cnh = cliente.cnh, Cep = cliente.cep, DataNascimento = cliente.data_nascimento, Telefone1 = cliente.telefone1, Telefone2 = cliente.telefone2 });
 
--- Classes/Ocorrencia/Services/InsertOcorrenciaService.cs
+++ Classes/Ocorrencia/Services/InsertOcorrenciaService.cs
@@ -8,37 +8,31 @@ static class InsertOcorrenciaService
   {
     SqlConnection connectionString = new SqlConnection(dbConnectionString);
 
+    // Fazendo o id_terceirizado pular a verificação de nulos.
+    int? originalId_Terceirizado = ocorrencia.id_terceirizado;
+    if (ocorrencia.id_terceirizado == null) ocorrencia.id_terceirizado = 0;
+
+    // Verificando se alguma das propriedades da ocorrência é nula ou vazia.
+    bool hasValidProperties = NullPropertyValidator.Validate(ocorrencia);
+    if (!hasValidProperties) return Results.BadRequest("Há um campo inválido na sua requisição.");
+
+    // Voltando terceirizado para o valor original.
+    ocorrencia.id_terceirizado = originalId_Terceirizado;
+
+    // Verificando se cliente existe no banco de dados.
+    bool clienteIsExistent = connectionString.QueryFirstOrDefault<bool>("SELECT id_cliente FROM Clientes WHERE id_cliente = @Id", new { Id = ocorrencia.id_cliente });
+    if (!clienteIsExistent) return Results.NotFound("Cliente não encontrado.");
+
+    // Verificando se veículo existe no banco de dados.
+    bool veiculoIsExistent = connectionString.QueryFirstOrDefault<bool>("SELECT id_veiculo FROM Veiculos WHERE id_veiculo = @Id", new { Id = ocorrencia.id_veiculo });
+    if (!veiculoIsExistent) return Results.NotFound("Veículo não encontrado.");
+
+    // Verificando se veículo pertence ao cliente.
+    bool veiculoIsValid = ClienteVeiculoValidator.Validate(ocorrencia.id_cliente, ocorrencia.id_veiculo, dbConnectionString);
+    if (!veiculoIsValid) return Results.BadRequest("Veículo não pertence ao cliente.");
+
     try
     {
-
-      // Fazendo o terceirizado pular a validação.
-      if (ocorrencia.id_terceirizado == null) ocorrencia.id_terceirizado = 0;
-
-      // Fazendo o documento pular a validação.
-      if (ocorrencia.documento == "" || ocorrencia.documento == null) ocorrencia.documento = "-";
-
-      // Verificando se alguma das propriedades da ocorrencia é nula ou vazia.
-      bool hasValidProperties = NullPropertyValidator.Validate(ocorrencia);
-      if (!hasValidProperties) return Results.BadRequest("Há um campo inválido na sua requisição.");
-
-      // Voltando terceirizado para o valor original.
-      ocorrencia.id_terceirizado = ocorrencia.id_terceirizado = null;
-
-      // Voltando documento para o valor original.
-      ocorrencia.documento = ocorrencia.documento.Replace("-", "");
-
-      // Verificando se cliente existe no banco de dados.
-      bool clienteIsExistent = connectionString.QueryFirstOrDefault<bool>("SELECT id_cliente FROM Clientes WHERE id_cliente = @Id", new { Id = ocorrencia.id_cliente });
-      if (!clienteIsExistent) return Results.NotFound("Cliente não encontrado.");
-
-      // Verificando se veículo existe no banco de dados.
-      bool veiculoIsExistent = connectionString.QueryFirstOrDefault<bool>("SELECT id_veiculo FROM Veiculos WHERE id_veiculo = @Id", new { Id = ocorrencia.id_veiculo });
-      if (!veiculoIsExistent) return Results.NotFound("Veículo não encontrado.");
-
-      // Verificando se veículo pertence ao cliente.
-      bool veiculoIsValid = ClienteVeiculoValidator.Validate(ocorrencia.id_cliente, ocorrencia.id_veiculo, dbConnectionString);
-      if (!veiculoIsValid) return Results.BadRequest("Veículo não pertence ao cliente.");
-
       connectionString.Query<Veiculo>("INSERT INTO Ocorrencias (data, local, UF, municipio, descricao, tipo, id_veiculo, id_cliente, id_terceirizado) VALUES (@Data, @Local, @UF, @Municipio, @Descricao, @Tipo,  @IdVeiculo, @IdCliente, @IdTerceirizado)", new { Data = ocorrencia.data, Local = ocorrencia.local, UF = ocorrencia.UF, Municipio = ocorrencia.municipio, Descricao = ocorrencia.descricao, Tipo = ocorrencia.tipo, IdVeiculo = ocorrencia.id_veiculo, IdCliente = ocorrencia.id_cliente, IdTerceirizado = ocorrencia.id_terceirizado });
 
       return Results.StatusCode(201);
