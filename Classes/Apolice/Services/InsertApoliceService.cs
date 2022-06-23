2f8825c4735727d8d10cf62c1ece7125d3a8f568
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-06-23 17:26:26 -0300

    feat: added endpoint tests

--- Classes/Apolice/Services/InsertApoliceService.cs
+++ Classes/Apolice/Services/InsertApoliceService.cs
@@ -8,6 +8,9 @@ static class InsertApoliceService
   {
     SqlConnection connectionString = new SqlConnection(dbConnectionString);
 
+    // Por padrão, o status da apólice é "Em Análise".
+    apolice.status = "Em Análise";
+
     // Verificando se alguma das propriedades do Veiculo é nula ou vazia.
     bool hasValidProperties = NullPropertyValidator.Validate(apolice);
     if (!hasValidProperties) return Results.BadRequest("Há um campo inválido na sua requisição.");
@@ -16,15 +19,13 @@ static class InsertApoliceService
     if (apolice.indenizacao == 0) return Results.BadRequest("Valor de indenização não pode ser 0.");
     if (apolice.premio == 0) return Results.BadRequest("Valor de prêmio não pode ser 0.");
 
-    // Por padrão, o status da apólice é "Em Análise".
-    apolice.status = "Em Análise";
 
     // Verificando se o cliente existe no banco de dados.
-    bool clienteExists = connectionString.QueryFirstOrDefault<bool>("SELECT id_cliente from Clientes WHERE id_cliente = @Id", new { Id = apolice.id_cliente });
+    bool clienteExists = connectionString.QueryFirstOrDefault<bool>("SELECT id_cliente from Clientes WHERE id_cliente = @Id AND status = 'true'", new { Id = apolice.id_cliente });
     if (!clienteExists) return Results.NotFound("Cliente não encontrado.");
 
     // Verificando se o veículo existe no banco de dados.
-    bool veiculoExists = connectionString.QueryFirstOrDefault<bool>("SELECT id_veiculo from Veiculos WHERE id_veiculo = @Id", new { Id = apolice.id_veiculo });
+    bool veiculoExists = connectionString.QueryFirstOrDefault<bool>("SELECT id_veiculo from Veiculos WHERE id_veiculo = @Id AND status = 'true'", new { Id = apolice.id_veiculo });
     if (!veiculoExists) return Results.NotFound("Veículo não encontrado.");
 
     // Verificando se o veículo realmente pertence ao cliente.
@@ -32,11 +33,11 @@ static class InsertApoliceService
     if (!veiculoIsValid) return Results.BadRequest("Veículo escolhido não pertence ao cliente.");
 
     // Verificando se a cobertura existe no banco de dados.
-    bool coberturaExists = connectionString.QueryFirstOrDefault<bool>("SELECT id_cobertura from Coberturas WHERE id_cobertura = @Id", new { Id = apolice.id_cobertura });
+    bool coberturaExists = connectionString.QueryFirstOrDefault<bool>("SELECT id_cobertura from Coberturas WHERE id_cobertura = @Id AND status = 'true'", new { Id = apolice.id_cobertura });
     if (!coberturaExists) return Results.NotFound("Cobertura não encontrada.");
 
     // Verificando se usuário existe no banco de dados.
-    bool usuarioExists = connectionString.QueryFirstOrDefault<bool>("SELECT id_usuario from Usuarios WHERE id_usuario = @Id", new { Id = apolice.id_usuario });
+    bool usuarioExists = connectionString.QueryFirstOrDefault<bool>("SELECT id_usuario from Usuarios WHERE id_usuario = @Id AND status = 'true'", new { Id = apolice.id_usuario });
     if (!usuarioExists) return Results.NotFound("Usuário não encontrado.");
 
     try
--- Utils/PolicyGenerator.cs
+++ Utils/PolicyGenerator.cs
@@ -18,7 +18,8 @@ static class PolicyGenerator
     SqlConnection connectionString = new SqlConnection(dbConnectionString);
 
     // Retornar o usuário que possuir menos apólices associadas à ele.
-    int leastApoliceUsuarioId = connectionString.QueryFirstOrDefault<int>("SELECT Usuarios.id_usuario, COUNT(Apolices.id_apolice) AS apolices FROM Usuarios JOIN Apolices ON Apolices.id_usuario = Usuarios.id_usuario GROUP BY Usuarios.id_usuario ORDER BY apolices ASC");
+    //TODO: Usuário com status inativo não deve ser escolhido.
+    int leastApoliceUsuarioId = connectionString.QueryFirstOrDefault<int>("SELECT Usuarios.id_usuario, COUNT(Apolices.id_apolice) AS apolices FROM Usuarios LEFT JOIN Apolices ON Apolices.id_usuario = Usuarios.id_usuario GROUP BY Usuarios.id_usuario ORDER BY apolices ASC");
 
     return leastApoliceUsuarioId;
 
