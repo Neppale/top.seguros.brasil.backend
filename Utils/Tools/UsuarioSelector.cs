36ea8ace278807eb58a72496099e71a5f1da4e32
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-06-25 01:01:44 -0300

    refactor: changed usuario selector query

--- Utils/Tools/UsuarioSelector.cs
+++ Utils/Tools/UsuarioSelector.cs
@@ -6,7 +6,7 @@ static class UsuarioSelector
 
     // Retornar o usuário que possuir menos apólices associadas à ele.
     //TODO: Usuário com status inativo não deve ser escolhido.
-    int leastApoliceUsuarioId = connectionString.QueryFirstOrDefault<int>("SELECT p.id_usuario, COUNT(o.id_apolice) AS apolices FROM Usuarios p LEFT JOIN Apolices o ON p.id_usuario = o.id_usuario WHERE p.status != 'false' GROUP BY p.id_usuario ORDER BY apolices ASC");
+    int leastApoliceUsuarioId = connectionString.QueryFirstOrDefault<int>("SELECT Usuarios.id_usuario, COUNT(Apolices.id_apolice) AS apolices FROM Usuarios LEFT JOIN Apolices ON Usuarios.id_usuario = Apolices.id_usuario WHERE Usuarios.status = 'true' GROUP BY Usuarios.id_usuario ORDER BY apolices ASC");
 
     return leastApoliceUsuarioId;
 
