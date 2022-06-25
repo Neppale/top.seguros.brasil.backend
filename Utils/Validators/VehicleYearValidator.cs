7aff16ebb694a287526298e6cf3fd5bf46975910
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-06-24 23:05:02 -0300

    feat: added list of approved fuel types to vehicle

--- Utils/Validators/VehicleYearValidator.cs
+++ Utils/Validators/VehicleYearValidator.cs
@@ -1,5 +1,6 @@
 static class VehicleYearValidator
 {
+  private static string[] approvedFuels = new string[] { "Gasolina", "Alcool", "Diesel", "Flex", "Tetrafuel", "Alcool/Gasolina" };
   /**<summary> Valida se o ano do veículo está no formato adequado. </summary>*/
   public static bool Validate(string year)
   {
