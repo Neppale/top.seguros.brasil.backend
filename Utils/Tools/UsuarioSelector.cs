496634c51ea93204d9ba5972e5a4b307f85e0607
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-06-24 22:57:31 -0300

    fix: usuario selector now doesn't select inactive usuarios

--- Utils/Tools/UsuarioSelector.cs
+++ Utils/Tools/UsuarioSelector.cs
@@ -6,7 +6,7 @@ static class UsuarioSelector
 
     // Retornar o usuário que possuir menos apólices associadas à ele.
     //TODO: Usuário com status inativo não deve ser escolhido.
-    int leastApoliceUsuarioId = connectionString.QueryFirstOrDefault<int>("SELECT Usuarios.id_usuario, COUNT(Apolices.id_apolice) AS apolices FROM Usuarios LEFT JOIN Apolices ON Apolices.id_usuario = Usuarios.id_usuario GROUP BY Usuarios.id_usuario ORDER BY apolices ASC");
+    int leastApoliceUsuarioId = connectionString.QueryFirstOrDefault<int>("SELECT p.id_usuario, COUNT(o.id_apolice) AS apolices FROM Usuarios p LEFT JOIN Apolices o ON p.id_usuario = o.id_usuario WHERE p.status != 'false' GROUP BY p.id_usuario ORDER BY apolices ASC");
 
     return leastApoliceUsuarioId;
 
