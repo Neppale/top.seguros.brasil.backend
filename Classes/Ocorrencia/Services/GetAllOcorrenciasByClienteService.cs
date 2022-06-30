64e4fb978e6484e5b81f1a3c4b37af2417a6ca4a
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-06-30 11:02:42 -0300

    fix: fixed pagination in get all ocorrencias by cliente service

--- Classes/Ocorrencia/Services/GetAllOcorrenciasByClienteService.cs
+++ Classes/Ocorrencia/Services/GetAllOcorrenciasByClienteService.cs
@@ -7,7 +7,7 @@ static class GetAllOcorrenciasByClienteService
     // Se pageNumber for nulo, então a página atual é a primeira.
     if (pageNumber == null) pageNumber = 1;
 
-    var data = connectionString.Query("SELECT id_ocorrencia, data, local, UF, municipio, descricao, tipo, Veiculos.modelo AS veiculo, Terceirizados.nome as terceirizado, Ocorrencias.status FROM Ocorrencias LEFT JOIN Veiculos ON Veiculos.id_veiculo = Ocorrencias.id_veiculo LEFT JOIN Terceirizados ON Terceirizados.id_terceirizado = Ocorrencias.id_terceirizado WHERE Ocorrencias.id_cliente = @IdCliente", new { PageNumber = (pageNumber - 1) * 5, IdCliente = id_cliente });
+    var data = connectionString.Query("SELECT id_ocorrencia, data, local, UF, municipio, descricao, tipo, Veiculos.modelo AS veiculo, Terceirizados.nome as terceirizado, Ocorrencias.status FROM Ocorrencias LEFT JOIN Veiculos ON Veiculos.id_veiculo = Ocorrencias.id_veiculo LEFT JOIN Terceirizados ON Terceirizados.id_terceirizado = Ocorrencias.id_terceirizado WHERE Ocorrencias.id_cliente = @IdCliente ORDER BY id_ocorrencia OFFSET @PageNumber ROWS FETCH NEXT 5 ROWS ONLY", new { PageNumber = (pageNumber - 1) * 5, IdCliente = id_cliente });
 
     return Results.Ok(data);
   }
