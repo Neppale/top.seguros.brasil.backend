34224997e83f5f7bf4434efbceeb141a4c59d436
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-06-30 23:55:41 -0300

    feat: get apolice endpoints now don't return documento

--- Classes/Apolice/Services/GetAllApoliceService.cs
+++ Classes/Apolice/Services/GetAllApoliceService.cs
@@ -8,7 +8,7 @@ static class GetAllApoliceService
     // se pageNumber for nulo, então a página atual é a primeira.
     if (pageNumber == null) pageNumber = 1;
 
-    var data = connectionString.Query<Apolice>("SELECT * from Apolices WHERE status != 'Rejeitada' ORDER BY id_apolice OFFSET @PageNumber ROWS FETCH NEXT 5 ROWS ONLY", new { PageNumber = (pageNumber - 1) * 5 });
+    var data = connectionString.Query<Apolice>("SELECT SELECT id_apolice, data_inicio, data_fim, premio, indenizacao, id_cobertura, id_usuario, id_cliente, id_veiculo, status from Apolices WHERE status != 'Rejeitada' ORDER BY id_apolice OFFSET @PageNumber ROWS FETCH NEXT 5 ROWS ONLY", new { PageNumber = (pageNumber - 1) * 5 });
 
     return Results.Ok(data);
   }
--- Classes/Apolice/Services/GetApolicesByClienteService.cs
+++ Classes/Apolice/Services/GetApolicesByClienteService.cs
@@ -7,7 +7,7 @@ static class GetApolicesByClienteService
     // Se pageNumber for nulo, então a página atual é a primeira.
     if (pageNumber == null) pageNumber = 1;
 
-    var data = connectionString.Query("SELECT * from Apolices WHERE id_cliente = @Id ORDER BY id_apolice OFFSET @PageNumber ROWS FETCH NEXT 5 ROWS ONLY", new { Id = id_cliente, PageNumber = (pageNumber - 1) * 5 });
+    var data = connectionString.Query("SELECT id_apolice, data_inicio, data_fim, premio, indenizacao, id_cobertura, id_usuario, id_cliente, id_veiculo, status from Apolices WHERE id_cliente = @Id ORDER BY id_apolice OFFSET @PageNumber ROWS FETCH NEXT 5 ROWS ONLY", new { Id = id_cliente, PageNumber = (pageNumber - 1) * 5 });
     if (data.Count() == 0) return Results.NotFound("Nenhuma apólice encontrada para o cliente, ou cliente não existe.");
 
     return Results.Ok(data);
