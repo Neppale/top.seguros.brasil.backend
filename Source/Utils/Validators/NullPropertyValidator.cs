c296b72b2c1982119a9498f1c11336468a1e0e82
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-08-17 13:59:05 -0300

    chore: cleaned up null property validator

--- Source/Utils/Validators/NullPropertyValidator.cs
+++ Source/Utils/Validators/NullPropertyValidator.cs
@@ -4,30 +4,17 @@ public static class NullPropertyValidator
 {
   public static bool Validate<Classe>(Classe data)
   {
-
     var properties = typeof(Classe).GetProperties();
-
     foreach (var property in properties)
     {
       var value = property.GetValue(data, null);
-
-      if (value == null)
-      {
-        return false;
-      }
+      if (value == null) return false;
     }
 
     foreach (var property in properties)
     {
       var value = property.GetValue(data, null);
-
-      if (value is string)
-      {
-        if (string.IsNullOrEmpty(value as string))
-        {
-          return false;
-        }
-      }
+      if (value is string) if (string.IsNullOrEmpty(value as string)) return false;
     }
 
     return true;
