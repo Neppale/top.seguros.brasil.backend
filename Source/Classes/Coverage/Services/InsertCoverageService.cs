8691137cb5423fb19e76c5b643da19a370b55a33
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-08-19 11:07:59 -0300

    feat: added validations to insert and update coverage services

--- Source/Classes/Coverage/Services/InsertCoverageService.cs
+++ Source/Classes/Coverage/Services/InsertCoverageService.cs
@@ -7,8 +7,12 @@ public static class InsertCoverageService
     if (!hasValidProperties) return Results.BadRequest(new { message = "Há um campo inválido na sua requisição." });
 
     cobertura.status = true;
+    double coverageValue = double.TryParse(cobertura.valor, out coverageValue) ? coverageValue : 0;
 
     if (cobertura.taxa_indenizacao <= 0) return Results.BadRequest(new { message = "Taxa de indenização não pode ser 0% ou menor." });
+    if (cobertura.taxa_indenizacao > 100) return Results.BadRequest(new { message = "Taxa de indenização não pode ser maior que 100%." });
+
+    if (coverageValue <= 0) return Results.BadRequest(new { message = "Valor da cobertura não pode ser 0 ou menor." });
 
     bool nameIsValid = CoverageAlreadyExistsValidator.Validate(name: cobertura.nome, connectionString: connectionString);
     if (!nameIsValid) return Results.Conflict(new { message = "O nome da cobertura já está sendo utilizado por outra cobertura ativa." });
--- Source/Classes/Coverage/Services/UpdateCoverageService.cs
+++ Source/Classes/Coverage/Services/UpdateCoverageService.cs
@@ -6,6 +6,13 @@ public static class UpdateCoverageService
     bool hasValidProperties = NullPropertyValidator.Validate(cobertura);
     if (!hasValidProperties) return Results.BadRequest(new { message = "Há um campo inválido na sua requisição." });
 
+    double coverageValue = double.TryParse(cobertura.valor, out coverageValue) ? coverageValue : 0;
+
+    if (cobertura.taxa_indenizacao <= 0) return Results.BadRequest(new { message = "Taxa de indenização não pode ser 0% ou menor." });
+    if (cobertura.taxa_indenizacao > 100) return Results.BadRequest(new { message = "Taxa de indenização não pode ser maior que 100%." });
+
+    if (coverageValue <= 0) return Results.BadRequest(new { message = "Valor da cobertura não pode ser 0 ou menor." });
+
     var coverage = GetOneCoverageService.Get(id: id, connectionString: connectionString);
     if (coverage == null) return Results.NotFound(new { message = "Cobertura não encontrada." });
 
