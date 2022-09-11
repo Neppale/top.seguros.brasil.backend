3e8b22bdf9a510728bda0dde04d6583ebbe3a08d
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-09-11 06:27:56 -0300

    refactor: refactored vehicle fipe validator

--- Source/Utils/Validators/VehicleFIPEValidator.cs
+++ Source/Utils/Validators/VehicleFIPEValidator.cs
@@ -6,45 +6,29 @@ static class VehicleFIPEValidator
   public static async Task<bool> Validate(string brand, string model, string year)
   {
     // Pesquisar por todos os veículos na API FIPE da mesma marca do veículo.
-    HttpResponseMessage brands = await client.GetAsync($"https://parallelum.com.br/fipe/api/v1/carros/marcas");
-    string brandsJson = await brands.Content.ReadAsStringAsync();
+    HttpResponseMessage brandsResponse = await client.GetAsync($"https://parallelum.com.br/fipe/api/v1/carros/marcas");
+    string brandsString = await brandsResponse.Content.ReadAsStringAsync();
+    var brands = JsonSerializer.Deserialize<VehicleBrands[]>(brandsString);
 
-    // Encontrar marca do veículo no texto do JSON.
-    Regex regex = new Regex($"{{\\\"nome\\\":\\\"{brand}\\\",\\\"codigo\\\":\\\"[0-9]+\\\"}}");
-    Match match = regex.Match(brandsJson);
-
-    // Encontrar o código da marca.
-    Regex regex2 = new Regex("[0-9]+");
-    Match foundBrandCode = regex2.Match(match.Value);
-    string brandCode = foundBrandCode.Value;
+    var foundBrand = brands?.FirstOrDefault(b => b.nome == brand);
+    if (foundBrand == null) return false;
 
     // Pesquisar na API por veículo com mesmo modelo.
-    HttpResponseMessage models = await client.GetAsync($"https://parallelum.com.br/fipe/api/v1/carros/marcas/{brandCode}/modelos");
-    string modelsJson = await models.Content.ReadAsStringAsync();
+    HttpResponseMessage modelsResponse = await client.GetAsync($"https://parallelum.com.br/fipe/api/v1/carros/marcas/{foundBrand.codigo}/modelos");
+    string modelsString = await modelsResponse.Content.ReadAsStringAsync();
+    var models = JsonSerializer.Deserialize<VehicleModels>(modelsString);
 
-    // Encontrar objeto com o modelo e código do veícudo na variável modelsJson
-    Regex regex3 = new Regex($"{{\\\"nome\\\":\\\"{model}\\\",\\\"codigo\\\":[0-9]+}}");
-    Match match2 = regex3.Match(modelsJson);
+    var foundModel = models?.modelos.FirstOrDefault(modelo => modelo.nome == model);
+    if (foundModel == null) return false;
 
-    // Encontrar o último numero dentro de 'match3'
-    Regex regex4 = new Regex("(\\d+)(?!.*\\d)");
-    Match foundModelCode = regex4.Match(match2.Value);
-    string modelCode = foundModelCode.Value;
+    // Pesquisar na API por veículo com mesmo ano.
+    HttpResponseMessage yearsResponse = await client.GetAsync($"https://parallelum.com.br/fipe/api/v1/carros/marcas/{foundBrand.codigo}/modelos/{foundModel.codigo}/anos");
+    string yearsString = await yearsResponse.Content.ReadAsStringAsync();
+    var years = JsonSerializer.Deserialize<VehicleYears[]>(yearsString);
 
-    // Pesquisar na API por veículo com mesmo ano de fabricação.
-    HttpResponseMessage years = await client.GetAsync($"https://parallelum.com.br/fipe/api/v1/carros/marcas/{brandCode}/modelos/{modelCode}/anos");
-    string yearsJson = await years.Content.ReadAsStringAsync();
+    var foundYear = years?.FirstOrDefault(y => y.nome == year);
+    if (foundYear == null) return false;
 
-    // Encontrar objeto com o ano de fabricação e código do veícudo na variável yearsJson
-    Regex regex5 = new Regex($"{{\\\"nome\\\":\\\"{year}\\\",\\\"codigo\\\":\\\"[0-9]+-[0-9]+\\\"}}");
-    Match match5 = regex5.Match(yearsJson);
-
-    // Encontrar o código dentro de 'match5'
-    Regex regex6 = new Regex("[0-9]+\\-[0-9]+");
-    Match foundYearCode = regex6.Match(match5.Value);
-    string yearCode = foundYearCode.Value;
-
-    if (brandCode == "" || modelCode == "" || yearCode == "") return false;
     return true;
   }
 }
