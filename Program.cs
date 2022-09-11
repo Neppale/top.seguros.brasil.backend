a30e03521b5f40156d5d6d5286a2b5206a0bc1bb
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-09-11 20:39:48 -0300

    fix: fixed connectionString variable name

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = APISetup.Setup(builder);
 
-string dbConnectionString = builder.Configuration["DefaultConnection"];
+string dbConnectionString = builder.Configuration["connectionString"];
 SqlConnection connectionString = new SqlConnection(dbConnectionString);
 connectionString.Open();
 
