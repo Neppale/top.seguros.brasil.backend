7657e570333cd703af5ab94fab6ce0727c1a3f71
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-09-27 12:45:56 -0300

    fix: fixed user password rules validation

--- Source/Classes/User/Services/UpdateUserService.cs
+++ Source/Classes/User/Services/UpdateUserService.cs
@@ -1,23 +1,26 @@
 static class UpdateUserService
 {
-    /** <summary> Esta função altera um Usuario no banco de dados. </summary>**/
-    public static async Task<IResult> Update(int id, Usuario usuario, SqlConnection connectionString)
-    {
-        bool hasValidProperties = NullPropertyValidator.Validate(usuario);
-        if (!hasValidProperties) return Results.BadRequest(new { message = "Há um campo inválido na sua requisição." });
+  /** <summary> Esta função altera um Usuario no banco de dados. </summary>**/
+  public static async Task<IResult> Update(int id, Usuario usuario, SqlConnection connectionString)
+  {
+    bool hasValidProperties = NullPropertyValidator.Validate(usuario);
+    if (!hasValidProperties) return Results.BadRequest(new { message = "Há um campo inválido na sua requisição." });
 
-        var user = await GetUserByIdRepository.Get(id: id, connectionString: connectionString);
-        if (user == null) return Results.NotFound(new { message = "Usuário não encontrado." });
+    bool passwordIsValid = PasswordValidator.Validate(usuario.senha);
+    if (!passwordIsValid) return Results.BadRequest("A senha informada não corresponde aos requisitos de segurança.");
 
-        bool userIsValid = await UserAlreadyExistsValidator.Validate(id: id, email: usuario.email, connectionString: connectionString);
-        if (!userIsValid) return Results.BadRequest(new { message = "O e-mail informado já está sendo utilizado em outra conta." });
+    var user = await GetUserByIdRepository.Get(id: id, connectionString: connectionString);
+    if (user == null) return Results.NotFound(new { message = "Usuário não encontrado." });
 
-        usuario.senha = PasswordHasher.HashPassword(usuario.senha);
+    bool userIsValid = await UserAlreadyExistsValidator.Validate(id: id, email: usuario.email, connectionString: connectionString);
+    if (!userIsValid) return Results.BadRequest(new { message = "O e-mail informado já está sendo utilizado em outra conta." });
 
-        var updatedUser = await UpdateUserRepository.Update(id: id, usuario: usuario, connectionString: connectionString);
-        if (updatedUser == null) return Results.BadRequest(new { message = "Houve um erro ao processar sua requisição. Tente novamente mais tarde." });
+    usuario.senha = PasswordHasher.HashPassword(usuario.senha);
 
-        return Results.Ok(new { message = "Usuário alterado com sucesso.", user = updatedUser });
-    }
+    var updatedUser = await UpdateUserRepository.Update(id: id, usuario: usuario, connectionString: connectionString);
+    if (updatedUser == null) return Results.BadRequest(new { message = "Houve um erro ao processar sua requisição. Tente novamente mais tarde." });
+
+    return Results.Ok(new { message = "Usuário alterado com sucesso.", user = updatedUser });
+  }
 }
 
