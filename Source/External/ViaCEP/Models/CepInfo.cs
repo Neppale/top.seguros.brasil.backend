01da3e6b4fd34c382ac5da0741ac0c9841656b5a
Gabriel Verissimo Dias <94950993+Neppale@users.noreply.github.com>
2022-09-27 22:55:15 -0300

    refactor: refactored document generation (#9)

---
+++ Source/External/ViaCEP/Models/CepInfo.cs
@@ -0,0 +1,13 @@
+﻿public class CepInfo
+{
+    public string cep { get; set; }
+    public string logradouro { get; set; }
+    public string complemento { get; set; }
+    public string bairro { get; set; }
+    public string localidade { get; set; }
+    public string uf { get; set; }
+    public string ibge { get; set; }
+    public string gia { get; set; }
+    public string ddd { get; set; }
+    public string siafi { get; set; }
+}
--- Source/Utils/Configuration/GlobalUses.cs
+++ Source/Utils/Configuration/GlobalUses.cs
@@ -1,5 +1,3 @@
-// Para evitar reescrever o código, é possível usar o global using.
-
 global using Dapper;
 global using System.Net;
 global using System.Text;
@@ -12,5 +10,5 @@ global using System.Text.RegularExpressions;
 global using Microsoft.IdentityModel.Tokens;
 global using System.IdentityModel.Tokens.Jwt;
 global using tsb.mininal.policy.engine.Utils;
-global using Microsoft.AspNetCore.Authentication.JwtBearer;
 global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
