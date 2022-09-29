9f58e8bed92bb9a6e1d05b1583bfaceaf6e9fe12
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-09-29 00:49:55 -0300

    fix: fixed policy document not being found

--- Source/Utils/Tools/PolicyDocumentGenerator.cs
+++ Source/Utils/Tools/PolicyDocumentGenerator.cs
@@ -1,63 +1,73 @@
 static class PolicyDocumentGenerator
 {
-    /**<summary> Esta função gera um arquivo HTML da apólice. </summary>**/
-    public static async Task<Stream> Generate(Apolice apolice, SqlConnection connectionString)
+  /**<summary> Esta função gera um arquivo HTML da apólice. </summary>**/
+  public static async Task<Stream> Generate(Apolice apolice, SqlConnection connectionString)
+  {
+    var user = await GetUserByIdRepository.Get(id: apolice.id_usuario, connectionString: connectionString);
+    var client = await GetClientByIdRepository.Get(id: apolice.id_cliente, connectionString: connectionString);
+    var vehicle = await GetVehicleByIdRepository.Get(id: apolice.id_veiculo, connectionString: connectionString);
+    var coverage = await GetCoverageByIdRepository.Get(id: apolice.id_cobertura, connectionString: connectionString);
+    var localization = await GetCepInfo.Get(client.cep);
+    decimal veiculoPreco = await VehiclePriceFinder.Find(vehicle.marca, vehicle.modelo, vehicle.ano);
+
+    string htmlDocument = await GetHtmlDocument();
+    htmlDocument = FormatHtmlDocument(apolice, user, client, vehicle, coverage, localization, veiculoPreco, htmlDocument);
+
+    var converter = new BasicConverter(new PdfTools());
+    var pdfDocument = new HtmlToPdfDocument()
     {
-        var user = await GetUserByIdRepository.Get(id: apolice.id_usuario, connectionString: connectionString);
-        var client = await GetClientByIdRepository.Get(id: apolice.id_cliente, connectionString: connectionString);
-        var vehicle = await GetVehicleByIdRepository.Get(id: apolice.id_veiculo, connectionString: connectionString);
-        var coverage = await GetCoverageByIdRepository.Get(id: apolice.id_cobertura, connectionString: connectionString);
-        var localization = await GetCepInfo.Get(client.cep);
-        decimal veiculoPreco = await VehiclePriceFinder.Find(vehicle.marca, vehicle.modelo, vehicle.ano);
-
-        string htmlDocument = await File.ReadAllTextAsync("./Source/Utils/Tools/Files/PolicyDocument.html");
-        htmlDocument = FormatHtmlDocument(apolice, user, client, vehicle, coverage, localization, veiculoPreco, htmlDocument);
-
-        var converter = new BasicConverter(new PdfTools());
-        var pdfDocument = new HtmlToPdfDocument()
-        {
-            GlobalSettings = {
-        ColorMode = ColorMode.Grayscale,
-        Orientation = Orientation.Portrait,
-        PaperSize = PaperKind.A4,
-    },
-            Objects = {
-        new ObjectSettings() {
-            PagesCount = true,
-            HtmlContent = htmlDocument,
-            WebSettings = { DefaultEncoding = "utf-8" },
-        }
+      GlobalSettings = {
+            ColorMode = ColorMode.Grayscale,
+            Orientation = Orientation.Portrait,
+            PaperSize = PaperKind.A4,
+        },
+      Objects = {
+            new ObjectSettings() {
+                PagesCount = true,
+                HtmlContent = htmlDocument,
+                WebSettings = { DefaultEncoding = "utf-8" },
+            }
     }
-        };
-        byte[] pdfDocumentBytes = converter.Convert(pdfDocument);
-        return new MemoryStream(pdfDocumentBytes);
-    }
+    };
+    byte[] pdfDocumentBytes = converter.Convert(pdfDocument);
+    return new MemoryStream(pdfDocumentBytes);
+  }
 
-    private static string FormatHtmlDocument(Apolice apolice, GetUserDto user, GetClientDto client, Veiculo vehicle, Cobertura coverage, CepInfo localization, decimal veiculoPreco, string htmlDocument)
+  private static async Task<string> GetHtmlDocument()
+  {
+    using (HttpClient httpClient = new())
     {
-        htmlDocument = htmlDocument.Replace("{{DATAHOJE}}", DateTime.Now.ToString("dd/MM/yyyy"))
-                                     .Replace("{{IDAPOLICE}}", apolice.id_apolice.ToString())
-                                     .Replace("{{DATAINICIAL}}", apolice.data_inicio.Substring(8, 2) + "/" + apolice.data_inicio.Substring(5, 2) + "/" + apolice.data_inicio.Substring(0, 4))
-                                     .Replace("{{DATAFINAL}}", apolice.data_fim.Substring(8, 2) + "/" + apolice.data_fim.Substring(5, 2) + "/" + apolice.data_fim.Substring(0, 4))
-                                     .Replace("{{NOMEUSUARIO}}", user.nome_completo)
-                                     .Replace("{{IDUSUARIO}}", user.id_usuario.ToString())
-                                     .Replace("{{NOMECLIENTE}}", client.nome_completo)
-                                     .Replace("{{CPFCLIENTE}}", client.cpf)
-                                     .Replace("{{CEPCLIENTE}}", client.cep)
-                                     .Replace("{{ENDERECOCLIENTE}}", $"{localization.logradouro}, {localization.bairro}, {localization.localidade}")
-                                     .Replace("{{UFCLIENTE}}", localization.uf)
-                                     .Replace("{{MARCAVEICULO}}", vehicle.marca)
-                                     .Replace("{{MODELOVEICULO}}", vehicle.modelo)
-                                     .Replace("{{PLACAVEICULO}}", vehicle.placa)
-                                     .Replace("{{COMBUSTIVELVEICULO}}", vehicle.ano[(vehicle.ano.IndexOf(" ") + 1)..])
-                                     .Replace("{{ANOVEICULO}}", vehicle.ano.Substring(0, vehicle.ano.IndexOf(" ")))
-                                     .Replace("{{USOVEICULO}}", vehicle.uso)
-                                     .Replace("{{DESCRICAOCOBERTURA}}", coverage.descricao)
-                                     .Replace("{{COBERTURAVALOR}}", coverage.valor.ToString())
-                                     .Replace("{{TAXAINDENIZACAOCOBERTURA}}", coverage.taxa_indenizacao.ToString())
-                                     .Replace("{{VALORVEICULOFIPE}}", veiculoPreco.ToString())
-                                     .Replace("{{PREMIOAPOLICE}}", apolice.premio.ToString())
-                                     .Replace("{{INDENIZACAOAPOLICE}}", apolice.indenizacao.ToString());
-        return htmlDocument;
+      HttpResponseMessage response = await httpClient.GetAsync($"https://verissimo.dev/api/PolicyDocument.html");
+      string htmlDocument = await response.Content.ReadAsStringAsync();
+      return htmlDocument;
     }
+  }
+
+  private static string FormatHtmlDocument(Apolice apolice, GetUserDto user, GetClientDto client, Veiculo vehicle, Cobertura coverage, CepInfo localization, decimal veiculoPreco, string htmlDocument)
+  {
+    htmlDocument = htmlDocument.Replace("{{DATAHOJE}}", DateTime.Now.ToString("dd/MM/yyyy"))
+                                 .Replace("{{IDAPOLICE}}", apolice.id_apolice.ToString())
+                                 .Replace("{{DATAINICIAL}}", apolice.data_inicio.Substring(8, 2) + "/" + apolice.data_inicio.Substring(5, 2) + "/" + apolice.data_inicio.Substring(0, 4))
+                                 .Replace("{{DATAFINAL}}", apolice.data_fim.Substring(8, 2) + "/" + apolice.data_fim.Substring(5, 2) + "/" + apolice.data_fim.Substring(0, 4))
+                                 .Replace("{{NOMEUSUARIO}}", user.nome_completo)
+                                 .Replace("{{IDUSUARIO}}", user.id_usuario.ToString())
+                                 .Replace("{{NOMECLIENTE}}", client.nome_completo)
+                                 .Replace("{{CPFCLIENTE}}", client.cpf)
+                                 .Replace("{{CEPCLIENTE}}", client.cep)
+                                 .Replace("{{ENDERECOCLIENTE}}", $"{localization.logradouro}, {localization.bairro}, {localization.localidade}")
+                                 .Replace("{{UFCLIENTE}}", localization.uf)
+                                 .Replace("{{MARCAVEICULO}}", vehicle.marca)
+                                 .Replace("{{MODELOVEICULO}}", vehicle.modelo)
+                                 .Replace("{{PLACAVEICULO}}", vehicle.placa)
+                                 .Replace("{{COMBUSTIVELVEICULO}}", vehicle.ano[(vehicle.ano.IndexOf(" ") + 1)..])
+                                 .Replace("{{ANOVEICULO}}", vehicle.ano.Substring(0, vehicle.ano.IndexOf(" ")))
+                                 .Replace("{{USOVEICULO}}", vehicle.uso)
+                                 .Replace("{{DESCRICAOCOBERTURA}}", coverage.descricao)
+                                 .Replace("{{COBERTURAVALOR}}", coverage.valor.ToString())
+                                 .Replace("{{TAXAINDENIZACAOCOBERTURA}}", coverage.taxa_indenizacao.ToString())
+                                 .Replace("{{VALORVEICULOFIPE}}", veiculoPreco.ToString())
+                                 .Replace("{{PREMIOAPOLICE}}", apolice.premio.ToString())
+                                 .Replace("{{INDENIZACAOAPOLICE}}", apolice.indenizacao.ToString());
+    return htmlDocument;
+  }
 }
--- Source/Utils/Tools/UserSelector.cs
+++ Source/Utils/Tools/UserSelector.cs
@@ -1,10 +1,10 @@
 static class UserSelector
 {
-    static public int Select(SqlConnection connectionString)
-    {
-        int userWithLeastPolicies = connectionString.QueryFirstOrDefault<int>("SELECT Usuarios.id_usuario, COUNT(Apolices.id_apolice) AS apolices FROM Usuarios LEFT JOIN Apolices ON Usuarios.id_usuario = Apolices.id_usuario WHERE Usuarios.status = 'true' AND Usuarios.tipo = 'Corretor' GROUP BY Usuarios.id_usuario ORDER BY apolices ASC");
+  static public async Task<int> Select(SqlConnection connectionString)
+  {
+    int userWithLeastPolicies = await connectionString.QueryFirstOrDefaultAsync<int>("SELECT Usuarios.id_usuario, COUNT(Apolices.id_apolice) AS apolices FROM Usuarios LEFT JOIN Apolices ON Usuarios.id_usuario = Apolices.id_usuario WHERE Usuarios.status = 'true' AND Usuarios.tipo = 'Corretor' GROUP BY Usuarios.id_usuario ORDER BY apolices ASC");
 
-        return userWithLeastPolicies;
+    return userWithLeastPolicies;
 
-    }
+  }
 }
