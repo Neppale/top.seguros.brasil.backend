e2d60fe024e075a4e21afb762b947d293f72b645
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-10-16 19:58:14 -0300

    fix: fixed lint

--- Source/Classes/Incident/Models/GetAllIncidentsDto.cs
+++ Source/Classes/Incident/Models/GetAllIncidentsDto.cs
@@ -1,22 +1,22 @@
 class GetAllIncidentsDto
 {
-  // Adaptado para o Management Stage.
-  public int id_ocorrencia { get; set; }
-  public string nome { get; set; }
-  public string tipo { get; set; }
-  public string data { get; set; }
-  public string status { get; set; }
+    // Adaptado para o Management Stage.
+    public int id_ocorrencia { get; set; }
+    public string nome { get; set; }
+    public string tipo { get; set; }
+    public string data { get; set; }
+    public string status { get; set; }
 
-  public GetAllIncidentsDto(int id_ocorrencia, string nome, string tipo, string data, string status)
-  {
-    this.id_ocorrencia = id_ocorrencia;
-    this.nome = nome;
-    this.tipo = tipo;
-    this.data = data;
-    this.status = status;
-  }
+    public GetAllIncidentsDto(int id_ocorrencia, string nome, string tipo, string data, string status)
+    {
+        this.id_ocorrencia = id_ocorrencia;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.data = data;
+        this.status = status;
+    }
 
-  GetAllIncidentsDto()
-  {
-  }
+    GetAllIncidentsDto()
+    {
+    }
 }
