e5a071e9eee0ea77dbb0f5577f11858c59c34142
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-10-16 19:56:29 -0300

    fix: reverted lots of stuff.

--- Source/Classes/Incident/Services/GetIncidentDocumentService.cs
+++ Source/Classes/Incident/Services/GetIncidentDocumentService.cs
@@ -1,16 +1,16 @@
 static class GetIncidentDocumentService
 {
-    /** <summary> Esta função retorna o documento de ocorrência específica no banco de dados. </summary>**/
-    public static async Task<IResult> Get(int id, SqlConnection connectionString)
-    {
-        var document = await GetIncidentDocumentRepository.Get(id: id, connectionString: connectionString);
-        if (document == null) return Results.NotFound(new { message = "Documento não encontrado." });
+  /** <summary> Esta função retorna o documento de ocorrência específica no banco de dados. </summary>**/
+  public static async Task<IResult> Get(int id, SqlConnection connectionString)
+  {
+    var document = await GetIncidentDocumentRepository.Get(id: id, connectionString: connectionString);
+    if (document.documento == null || document.tipoDocumento == null) return Results.NotFound(new { message = "Documento não encontrado." });
 
-        var documentStream = DocumentConverter.Decode(document.documento, document.tipoDocumento);
+    var documentStream = DocumentConverter.Decode(document.documento, document.tipoDocumento);
 
-        string fileType = document.tipoDocumento.Split('/')[1];
-        string fileName = $"Ocorrencia-{Guid.NewGuid()}.{fileType}";
+    string fileType = document.tipoDocumento.Split('/')[1];
+    string fileName = $"Ocorrencia-{Guid.NewGuid()}.{fileType}";
 
-        return Results.File(documentStream, contentType: document.tipoDocumento, fileDownloadName: fileName);
-    }
+    return Results.File(documentStream, contentType: document.tipoDocumento, fileDownloadName: fileName);
+  }
 }
