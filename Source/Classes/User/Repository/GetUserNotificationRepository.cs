b6aa6a680e6633196f9bb7e16dd84fb119c954b7
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-10-20 21:40:55 -0300

    fix: fixed lint

--- Source/Classes/User/Repository/GetUserNotificationRepository.cs
+++ Source/Classes/User/Repository/GetUserNotificationRepository.cs
@@ -1,8 +1,8 @@
 static class GetUserNotificationRepository
 {
-  public static async Task<int> Get(int id, SqlConnection connectionString)
-  {
-    var amount = await connectionString.QueryFirstOrDefaultAsync<int>("SELECT quantidade FROM Notificacoes WHERE id_usuario = @id", new { id });
-    return amount;
-  }
+    public static async Task<int> Get(int id, SqlConnection connectionString)
+    {
+        var amount = await connectionString.QueryFirstOrDefaultAsync<int>("SELECT quantidade FROM Notificacoes WHERE id_usuario = @id", new { id });
+        return amount;
+    }
 }
