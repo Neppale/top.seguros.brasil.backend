7f05eb10adb74a4b36bf62a13bc9695f37afd15e
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-10-28 22:08:48 -0300

    fix: user notification is now correct

--- Source/Utils/Tools/IndemnisationCalculator.cs
+++ Source/Utils/Tools/IndemnisationCalculator.cs
@@ -1,20 +1,13 @@
 static class IndemnisationCalculator
 {
-    /** <summary> Esta função gera um valor de indenização. </summary>**/
-    public static async Task<decimal> Calculate(int id_cobertura, decimal vehicleValue, SqlConnection connectionString)
-    {
-        // TODO: This is wrong on production. Log every step of the process.
-        var coverage = await GetCoverageByIdRepository.Get(id: id_cobertura, connectionString: connectionString);
-        decimal indemnisationTax = coverage.taxa_indenizacao;
-        Console.WriteLine("Indemnisation tax: " + indemnisationTax + "%");
+  /** <summary> Esta função gera um valor de indenização. </summary>**/
+  public static async Task<decimal> Calculate(int id_cobertura, decimal vehicleValue, SqlConnection connectionString)
+  {
+    var coverage = await GetCoverageByIdRepository.Get(id: id_cobertura, connectionString: connectionString);
+    decimal indemnisationTax = coverage.taxa_indenizacao;
 
-        // Indemnisation tax is a percentage, so we need to divide it by 100. Indemnisation value is the vehicle value multiplied by the indemnisation tax.
-        decimal indemnisationValue = (vehicleValue * (indemnisationTax / 100));
-        Console.WriteLine("Indemnisation value: " + indemnisationValue);
-
-        indemnisationValue = Math.Round(indemnisationValue, 2);
-        Console.WriteLine("Indemnisation value rounded: " + indemnisationValue);
-
-        return indemnisationValue;
-    }
+    decimal indemnisationValue = (vehicleValue * (indemnisationTax / 100));
+    indemnisationValue = Math.Round(indemnisationValue, 2);
+    return indemnisationValue;
+  }
 }
--- Source/Utils/Tools/PremiumCalculator.cs
+++ Source/Utils/Tools/PremiumCalculator.cs
@@ -1,19 +1,13 @@
 static class PremiumCalculator
 {
-    /** <summary> Esta função gera um valor de prêmio. </summary>**/
-    public static async Task<decimal> Calculate(decimal vehicleValue, int id_cobertura, SqlConnection connectionString)
-    {
-        var coverage = await GetCoverageByIdRepository.Get(id: id_cobertura, connectionString: connectionString);
-        var coverageValue = coverage.valor;
-        Console.WriteLine("Coverage value: " + coverageValue);
+  /** <summary> Esta função gera um valor de prêmio. </summary>**/
+  public static async Task<decimal> Calculate(decimal vehicleValue, int id_cobertura, SqlConnection connectionString)
+  {
+    var coverage = await GetCoverageByIdRepository.Get(id: id_cobertura, connectionString: connectionString);
+    var coverageValue = coverage.valor;
 
-        decimal premiumValue = (((vehicleValue) * 0.005m) + coverageValue);
-        Console.WriteLine("Vehicle value: " + vehicleValue);
-        Console.WriteLine("Premium value: " + premiumValue);
-
-        premiumValue = Math.Round(premiumValue, 2);
-        Console.WriteLine("Premium value rounded: " + premiumValue);
-
-        return premiumValue;
-    }
+    decimal premiumValue = (((vehicleValue) * 0.005m) + coverageValue);
+    premiumValue = Math.Round(premiumValue, 2);
+    return premiumValue;
+  }
 }
