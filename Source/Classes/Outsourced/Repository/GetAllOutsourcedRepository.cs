8055a43bb93fdca1c35c9dbbd133ee33f331c541
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-10-29 01:03:05 -0300

    feat: added total pages to incidents

--- Source/Classes/Outsourced/Repository/GetAllOutsourcedRepository.cs
+++ Source/Classes/Outsourced/Repository/GetAllOutsourcedRepository.cs
@@ -2,9 +2,12 @@ static class GetAllOutsourcedRepository
 {
   public static async Task<PaginatedOutsourced> Get(SqlConnection connectionString, int? pageNumber, int? size)
   {
+    if (size == null) size = 5;
+
     Terceirizado[] outsourceds;
     outsourceds = (await connectionString.QueryAsync<Terceirizado>("SELECT * from Terceirizados WHERE status = 'true' ORDER BY id_terceirizado DESC OFFSET @PageNumber ROWS FETCH NEXT @Size ROWS ONLY", new { PageNumber = (pageNumber - 1) * size, Size = size })).ToArray();
-    var totalPages = await connectionString.QueryFirstAsync<int>("SELECT COUNT(*) FROM Terceirizados WHERE status = 'true'");
+    var outsourcedCount = await connectionString.QueryFirstOrDefaultAsync<int>("SELECT COUNT(*) FROM Terceirizados WHERE status = 'true'");
+    var totalPages = (int)Math.Ceiling((double)outsourcedCount / (int)size);
 
     var paginatedOutsourced = new PaginatedOutsourced(outsourceds: outsourceds, totalPages: totalPages);
 
