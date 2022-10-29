9b4e4713eb0ff9e97182f3d9defedb31f002edb1
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-10-28 22:01:35 -0300

    fix: user notification is now correct

--- Source/Classes/User/Repository/NotifyUserRepository.cs
+++ Source/Classes/User/Repository/NotifyUserRepository.cs
@@ -3,7 +3,7 @@ static class NotifyUserRepository
   /** <summary> Esta função notifica o usuário sobre a criação de uma nova apólice. </summary>**/
   public static async Task Notify(int id, SqlConnection connectionString)
   {
-    var userNotification = await connectionString.QueryFirstOrDefaultAsync<int>("SELECT notificacoes FROM usuario WHERE id = @id", new { id = id });
+    var userNotification = await connectionString.QueryFirstOrDefaultAsync<int>("SELECT notificacoes FROM Usuarios WHERE id = @id", new { id = id });
     await connectionString.QueryAsync("UPDATE Usuarios SET notificacoes = @notifications WHERE id = @id", new { notifications = userNotification + 1, id = id });
   }
 }
