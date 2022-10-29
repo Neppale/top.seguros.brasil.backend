0b8dcb6ac354e38786a1f265b3c5807c749a2e9d
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-10-29 00:45:05 -0300

    feat: added total pages to outsourced service

--- Source/Classes/Outsourced/Repository/GetAllOutsourcedRepository.cs
+++ Source/Classes/Outsourced/Repository/GetAllOutsourcedRepository.cs
@@ -1,7 +1,13 @@
 static class GetAllOutsourcedRepository
 {
-    public static async Task<IEnumerable<Terceirizado>> Get(SqlConnection connectionString, int? pageNumber, int? size)
-    {
-        return await connectionString.QueryAsync<Terceirizado>("SELECT * from Terceirizados WHERE status = 'true' ORDER BY id_terceirizado DESC OFFSET @PageNumber ROWS FETCH NEXT @Size ROWS ONLY", new { PageNumber = (pageNumber - 1) * size, Size = size });
-    }
+  public static async Task<PaginatedOutsourced> Get(SqlConnection connectionString, int? pageNumber, int? size)
+  {
+    Terceirizado[] outsourceds;
+    outsourceds = (await connectionString.QueryAsync<Terceirizado>("SELECT * from Terceirizados WHERE status = 'true' ORDER BY id_terceirizado DESC OFFSET @PageNumber ROWS FETCH NEXT @Size ROWS ONLY", new { PageNumber = (pageNumber - 1) * size, Size = size })).ToArray();
+    var totalPages = await connectionString.QueryFirstAsync<int>("SELECT COUNT(*) FROM Terceirizados WHERE status = 'true'");
+
+    var paginatedOutsourced = new PaginatedOutsourced(outsourceds: outsourceds, totalPages: totalPages);
+
+    return paginatedOutsourced;
+  }
 }
