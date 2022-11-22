ad4fd7b86e92fd0b046fb74da7301af918d88469
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-11-21 23:00:38 -0300

    fix: am I dumb?

--- Source/Utils/Tools/SqlDateConverter.cs
+++ Source/Utils/Tools/SqlDateConverter.cs
@@ -18,10 +18,13 @@ static class SqlDateConverter
     /* <summary> Esta função converte uma data no formato brasileiro para o formato SQL. </summary> */
     public static string ConvertToSave(string date)
     {
+        var regex = new Regex(@"^\d{4}-\d{2}-\d{2}$");
+        if (!regex.IsMatch(date)) return "0000-00-00";
+
         var dateParts = date.Split('-');
-        var month = dateParts[0];
-        var day = dateParts[1];
-        var year = dateParts[2];
+        var year = dateParts[0];
+        var month = dateParts[1];
+        var day = dateParts[2];
 
         if (int.Parse(month) > 12) return "0000-00-00";
 
