91b0512b13e428629673519675e2bb77f2867181
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-11-21 22:27:34 -0300

    fix: fixed date in incidents that are not found

--- Source/Classes/Incident/Services/GetAllIncidentService.cs
+++ Source/Classes/Incident/Services/GetAllIncidentService.cs
@@ -1,16 +1,14 @@
 static class GetAllIncidentService
 {
-    /** <summary> Esta função retorna todas as ocorrências no banco de dados. </summary>**/
-    public static async Task<IResult> Get(SqlConnection connectionString, int? pageNumber, int? size, string? search)
-    {
-        if (pageNumber == null) pageNumber = 1;
-        if (size == null) size = 5;
+  /** <summary> Esta função retorna todas as ocorrências no banco de dados. </summary>**/
+  public static async Task<IResult> Get(SqlConnection connectionString, int? pageNumber, int? size, string? search)
+  {
+    if (pageNumber == null) pageNumber = 1;
+    if (size == null) size = 5;
 
-        var incidents = await GetAllIncidentRepository.Get(connectionString: connectionString, pageNumber: pageNumber, size: size, search: search);
-        // for each incident, change the date format in property data from MM/dd/yyyy to dd/MM/yyyy using regex
-        foreach (var incident in incidents.incidents) incident.data = Regex.Replace(incident.data, @"(\d{2})/(\d{2})/(\d{4})", "$2/$1/$3");
-        var paginatedResponse = new paginatedResponse(data: incidents.incidents, totalPages: incidents.totalPages);
+    var incidents = await GetAllIncidentRepository.Get(connectionString: connectionString, pageNumber: pageNumber, size: size, search: search);
+    var paginatedResponse = new paginatedResponse(data: incidents.incidents, totalPages: incidents.totalPages);
 
-        return Results.Ok(paginatedResponse);
-    }
+    return Results.Ok(paginatedResponse);
+  }
 }
--- Source/Classes/Incident/Services/GetIncidentByClientService.cs
+++ Source/Classes/Incident/Services/GetIncidentByClientService.cs
@@ -1,14 +1,13 @@
 static class GetIncidentByClientService
 {
-    public static async Task<IResult> Get(int id_cliente, SqlConnection connectionString, int? pageNumber, int? size)
-    {
-        if (pageNumber == null) pageNumber = 1;
-        if (size == null) size = 5;
+  public static async Task<IResult> Get(int id_cliente, SqlConnection connectionString, int? pageNumber, int? size)
+  {
+    if (pageNumber == null) pageNumber = 1;
+    if (size == null) size = 5;
 
-        var incidents = await GetIncidentByClientRepository.Get(id: id_cliente, connectionString: connectionString, pageNumber: pageNumber, size: size);
-        foreach (var incident in incidents.incidents) incident.data = Regex.Replace(incident.data, @"(\d{2})/(\d{2})/(\d{4})", "$2/$1/$3");
-        var paginatedResponse = new paginatedResponse(data: incidents.incidents, totalPages: incidents.totalPages);
+    var incidents = await GetIncidentByClientRepository.Get(id: id_cliente, connectionString: connectionString, pageNumber: pageNumber, size: size);
+    var paginatedResponse = new paginatedResponse(data: incidents.incidents, totalPages: incidents.totalPages);
 
-        return Results.Ok(paginatedResponse);
-    }
+    return Results.Ok(paginatedResponse);
+  }
 }
